d5b10ef153cfe445a5c9925d603353edccb27aa4
zeyadShapan <zeyadshapan2004@gmail.com>
2021-09-20 10:46:06 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+chrome.browserAction.onClicked.addListener(function (tab) {
+    const url = window.location.href;
+    const startIndex = url.indexOf('=') + 1;
+    const endIndex = url.indexOf('&') === -1 ? url.length + 1 : url.indexOf('&');
+
+    const id = url.slice(startIndex, endIndex);
+    const embedUrl = `https://youtube.com/embed/${id}?autoplay=1`;
+    window.location.href = embedUrl;
+
+    chrome.tabs.update(tab.id, {
+        url: embedUrl
+    });
+});
